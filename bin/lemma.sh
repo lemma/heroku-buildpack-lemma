ef147aedba4e73221831e293e44b18d404ae6e68
Ben Burkert <ben@benburkert.com>
2014-11-29 17:40:11 -0800

    Use the LEMMA_DYNO_TYPE environment variable to enable lemmad

--- bin/lemma.sh
+++ bin/lemma.sh
@@ -1,7 +1,16 @@
-#!/usr/bin/env bash -e
+#!/usr/bin/env bash
 #
 
 dir="$(dirname ${BASH_SOURCE[0]})"
+dyno="${DYNO%.*}"
+ctypes="${LEMMA_DYNO_TYPE:-web}"
+types=$(echo $ctypes | tr "," "\n")
 
-chmod +x $dir/lemmad
-$dir/lemmad &
+for dtype in $types ; do
+  if [[ "$dyno" == "$dtype" ]] ; then
+    chmod +x $dir/lemmad
+    $dir/lemmad &
+
+    exit 0
+  fi
+done
